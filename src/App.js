c89de935003a7f173872441b8def4f7ef2c475fd
taehyeong park <b_tae96@naver.com>
2022-07-05 11:01:54 +0900

    update . apexcharts를 이용한 캔들 차트 , 간단 옵션 정리, 이쁜 디자인 X

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Chart1 from "./apexcharts-candle/Chart1";
+import Chart2 from "./apexcharts-candle/Chart2";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Chart1 />
+      <Chart2 />
     </div>
   );
 }
